515a912db7b435e5b1a463921f9179b1bdc639c9
albiummid <albi.ummid@gmail.com>
2024-09-16 23:56:44 +0600

    NEXT_PUBIC_ removed

--- env.app.ts
+++ env.app.ts
@@ -1,12 +1,11 @@
-
 import { cleanEnv, str } from "envalid";
 
-export const env = cleanEnv(process?.env,{
-    NEXT_PUBLIC_MONGO_DB_URI:str(),
-    NEXT_PUBLIC_FIREBASE_API_KEY :str(),
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN :str(),
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID :str(),
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET :str(),
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID :str(),
-    NEXT_PUBLIC_FIREBASE_APP_ID :str(),
-})
+export const env = cleanEnv(process?.env, {
+    MONGO_DB_URI: str(),
+    FIREBASE_API_KEY: str(),
+    FIREBASE_AUTH_DOMAIN: str(),
+    FIREBASE_PROJECT_ID: str(),
+    FIREBASE_STORAGE_BUCKET: str(),
+    FIREBASE_MESSAGING_SENDER_ID: str(),
+    FIREBASE_APP_ID: str(),
+});
--- src/database/db.ts
+++ src/database/db.ts
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 import { env } from "../../env.app";
 
-let connection:{isConnected?:number} = {};
+let connection: { isConnected?: number } = {};
 
 async function dbConnect() {
-    if(connection.isConnected){
+    if (connection.isConnected) {
         return;
     }
-    try{
-        const db = await mongoose.connect(env.NEXT_PUBLIC_MONGO_DB_URI);
-        connection={isConnected : db.connections[0].readyState}
-        console.log(`⚡ DB_CONNECTED`)
-    }catch(err){
-        console.log(`DB_ERROR: Connection couldn't established.\nDetails: ${JSON.stringify(err)}`)
+    try {
+        const db = await mongoose.connect(env.MONGO_DB_URI);
+        connection = { isConnected: db.connections[0].readyState };
+        console.log(`⚡ DB_CONNECTED`);
+    } catch (err) {
+        console.log(
+            `DB_ERROR: Connection couldn't established.\nDetails: ${JSON.stringify(
+                err
+            )}`
+        );
     }
 }
 
-export default dbConnect
+export default dbConnect;
 
 export interface Timestamp {
-    createdAt:Date;
-    updatedAt:Date;
+    createdAt: Date;
+    updatedAt: Date;
 }
-
