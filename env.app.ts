b1342241dbfa61a42a95d3f62201fc468e3ba573
albiummid <albi.ummid@gmail.com>
2024-09-17 00:06:06 +0600

    changes

--- env.app.ts
+++ env.app.ts
@@ -2,7 +2,7 @@ import { cleanEnv, str } from "envalid";
 
 export const env = cleanEnv(process?.env, {
     MONGO_DB_URI: str(),
-    FIREBASE_API_KEY: str(),
+    NEXT_PUBLIC_FIREBASE_API_KEY: str(),
     FIREBASE_AUTH_DOMAIN: str(),
     FIREBASE_PROJECT_ID: str(),
     FIREBASE_STORAGE_BUCKET: str(),
--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -5,7 +5,7 @@ import { getApps, initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
